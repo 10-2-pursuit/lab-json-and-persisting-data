3b54f77c995682752cab619f8dce682707f578e1
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-06-04 17:37:29 -0400

    update some test ccases

--- __test__/fs.test.js
+++ __test__/fs.test.js
@@ -1,6 +1,25 @@
 const { writeJSON, readJSON } = require("../src/fs.js");
 
 describe('readJSON', () => {
-    const actual;
+    const mock = require('mock-fs');
+    mock({
+        'folderName': {
+            'index.json': '{"_id":"d3156ec1-944f-4a25-afef-ca2811b78278"}'
+        },
+    })
+    const file = `index.json`;
+    const path = `${process.cwd()}/folderName`;
+    const actual = readJSON(path, file);
     
+    test("opening file properly", ()=>{
+        expect(actual).toStrictEqual({"_id":"d3156ec1-944f-4a25-afef-ca2811b78278"});
+    })
+    test("if file does not exist, catch ENOENT error", ()=>{
+        try{
+            const actual2 = readJSON(path, "NOTHING");
+        } catch (e) {
+            expect(e.code).toEqual('ENOENT');
+        }
+    })
+    mock.restore();
 });
