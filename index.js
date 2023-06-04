d008fcff51446e986bef43220899a12906779e59
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-06-04 17:58:16 -0400

    added some comments

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const { writeJSON, readJSON } = require("./src/fs.js");
 const { createArrF } = require("./src/gen.js");
 const _ = require('lodash');
 
-
+/**
+ * run()
+ * -------------------------
+ * default function to handle argv & functions.
+ * 
+ */
 function run(){
     const argc = process.argv.length;
     if(argc == 2){
@@ -15,7 +20,8 @@ function run(){
         writeJSON("./data","data.json", temp);
     }
     else{
-        console.log("HI");
+        console.log("node index.js (number) : will create (number) objects to JSON file");
+        console.log("node index.js : read & log JSON file");
     }
 }
 
