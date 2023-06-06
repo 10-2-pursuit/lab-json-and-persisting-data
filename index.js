3f683370e83d97736d22e9cdba6902ac35a5fae9
Anamaria Torres <anamariat528@icloud.com>
2023-06-06 09:19:04 -0400

    added functions

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-  //check to see if the generated name exists inside of the array
-            //log username in our data set
-            //if it does not exist, return username
+const {
+    createUserName,
+    checkForDuplicates} = require("./usergeneration.js")
+const { writeJSONFile, readJSONFile } = require("./helper");
+
+function run(){
+    const username = readJSONFile("userData", "userNameInfo.json");
+            // console.log(username)
+    if(process.argv[2]){
+        username.push(...checkForDuplicates(process.argv[2]))
+    } 
+    else {
+        username.push(createUserName())
+    }
+    writeJSONFile("userData", "userNameInfo.json", username)
+}
+                
+run()
+
+
+// var args = process.argv;
+// console.log("Total number of arguments are: "+args.length);
+// args.forEach((val, index) => {
+// console.log(`${index}: ${val}`);
+// });
--- usergeneration.js
+++ usergeneration.js
@@ -1,4 +1,4 @@
-const userNameArray = require("./userData/userNameInfo");
+const userNameData = require("./userData/userNameInfo");
 const {faker} = require("@faker-js/faker");
 // createas a random generated name
 function randomlyGeneratedName() {
@@ -21,22 +21,40 @@ function createUserName(input) {
         return input
     }
 }
-console.log(1, createUserName())
-console.log(2, createUserName("pickledTomato"))
-
-//returns a random animal of a random sex
-function randomAnimal(){
-    const animals = {
-        _animalName: faker.animal.type(),
-        _gender: faker.person.sex()
-    }
-     return animals
-    }
-    console.log(randomAnimal())
+// console.log(1, createUserName())
+// console.log(2, createUserName("pickledTomato"))
 
 
-    module.exports = {
+//check to see if the generated/user input name exists inside of the array
+    //if it does not exist, log username in our data set
+    //if it does exist, return error message
+function checkForDuplicates(userNameData, input, createUserName) {
+    let username = createUserName(input)
+    if (userNameData.includes(username)) {
+            return "Username is taken"
+        }
+     else //userNameData.push(username)
+        return userNameData
+}
+// console.log(1, checkForDuplicates(userNameData, "testerName", createUserName))
+// console.log(2, checkForDuplicates(userNameData, "pinkKoala", createUserName))
+// console.log(3, checkForDuplicates(userNameData, null, createUserName))
+
+
+// //returns a random animal of a random sex
+// function randomAnimal(){
+//     const animals = {
+//         _animalName: faker.animal.type(),
+//         _gender: faker.person.sex()
+//     }
+//      return animals
+//     }
+//     // console.log(randomAnimal())
+
+
+module.exports = {
         randomlyGeneratedName,
         createUserName,
-        randomAnimal
-    }
+        // randomAnimal,
+        checkForDuplicates
+}
